dc5e404d5952eb6dd0abab993580078ab954899e
SnaakZA <snaakza@gmail.com>
2018-12-14 07:27:21 +0200

    COP Level 1 - 3 Complete

--- Altis_Life.Altis/config/Config_Weapons.hpp
+++ Altis_Life.Altis/config/Config_Weapons.hpp
@@ -161,23 +161,19 @@ class WeaponShops {
             { "ItemGPS", "", 100, 50, "" },
             { "FirstAidKit", "", 150, 75, "" },
             { "NVGoggles", "", 2000, 1000, "" },
+            { "SmokeShell", "", 2000, 1000, "" },
             { "HandGrenade_Stone", $STR_W_items_Flashbang, 1700, 850, "" },
             { "hgun_P07_snds_F", $STR_W_items_StunPistol, 2000, 1000, "" },
             { "arifle_SDAR_F", $STR_W_items_TaserRifle, 20000, 10000, "" },
-            
-            
-            
+    
             
             //================================
             //============ CADET  ============
             //================================
-            
-            
+
             { "SMG_02_F", "", 30000, 1500, "call life_coplevel >= 1" },         
             
-            
-            
-                
+
             //================================
             //============ CORPORAL ==========
             //================================
@@ -187,7 +183,20 @@ class WeaponShops {
             { "arifle_CTAR_blk_F", "", 6500, , "call life_coplevel >= 2" },     
             { "CSW_FN57", "", 5500, , "call life_coplevel >= 2" },      
             { "SatchelCharge_Remote_Mag", "", 5500, , "call life_coplevel >= 2" },      
-            { "Rangefinder", "", 5500, , "call life_coplevel >= 2" }        
+            { "Rangefinder", "", 5500, , "call life_coplevel >= 2" },       
+
+            //================================
+            //============ SERGEANT ==========
+            //================================  
+    
+            { "arifle_CTAR_GL_blk_F", "", 12000, , "call life_coplevel >= 2" }, 
+            { "arifle_CTARS_blk_F", "", 14500, , "call life_coplevel >= 2" },   
+            { "srifle_DMR_07_blk_F", "", 14500, , "call life_coplevel >= 2" },  
+            { "CSW_FN57", "", 3000, , "call life_coplevel >= 2" },          
+            { "SatchelCharge_Remote_Mag", "", 3000, , "call life_coplevel >= 2" },
+
+
+
 
     
     
@@ -216,14 +225,19 @@ class WeaponShops {
             { "50Rnd_570x28_SMG_03", "", 200, , "call life_coplevel >= 2" },                
             { "30Rnd_580x42_Mag_F", "", 200, , "call life_coplevel >= 2" },
             { "prpl_8Rnd_12Gauge_Pellets", "", 200, , "call life_coplevel >= 2" },          
-            { "prpl_8Rnd_12Gauge_Pellets", "", 200, , "call life_coplevel >= 2" },                      
-            { "CSW_20Rnd_57x28_SS190", "", 200, , "call life_coplevel >= 2" }                   
+            { "prpl_8Rnd_12Gauge_Slug", "", 200, , "call life_coplevel >= 2" },                     
+            { "CSW_20Rnd_57x28_SS190", "", 200, , "call life_coplevel >= 2" },                  
             
+            //================================
+            //============ SERGEANT ==========
+            //================================
 
-
-            
-            
-            
+            { "100Rnd_580x42_Mag_F", "", 450, , "call life_coplevel >= 2" },
+            { "20Rnd_650x39_Cased_Mag_F", "", 350, , "call life_coplevel >= 2" },
+            { "1Rnd_Smoke_Grenade_shell", "", 80, , "call life_coplevel >= 2" },
+            { "UGL_FlareCIR_F", "", 80, , "call life_coplevel >= 2" },
+            { "UGL_FlareWhite_F", "", 80, , "call life_coplevel >= 2" }
+    
             
             
             
@@ -231,13 +245,16 @@ class WeaponShops {
             
         };
         accs[] = {
+            
+            
+            //================================
+            //============ ALL RANKS =========
+            //================================
+
+            
             { "muzzle_snds_L", "", 650, 325, "" },
-            { "optic_MRD", "", 2750, 1375, "call life_coplevel >= 1" },
-            { "acc_flashlight_pistol", "", 250, 125, "call life_coplevel >= 1" },//Pistol Flashlight
-            { "acc_flashlight", "", 750, 375, "call life_coplevel >= 2" },
-            { "optic_Holosight", "", 1200, 600, "call life_coplevel >= 2" },
-            { "optic_Arco", "", 2500, 1250, "call life_coplevel >= 2" },
-            { "muzzle_snds_H", "", 2750, 1375, "call life_coplevel >= 2" },
+
+  
             
             
             
@@ -245,11 +262,10 @@ class WeaponShops {
             //============ CADET  ============
             //================================
             
-            { "muzzle_snds_L", "Suppressor 9mm", 650, 325, "" },
-            { "optic_Aco_smg", "ACO", 2750, 1375, "" },
-            { "acc_flashlight", "Gun Light", 750, 375, "" },
-            { "KA_ANPEQ15_Black_IR", "Laser", 1200, 600, "" },
-            
+            { "optic_MRD", "", 2750, 1375, "call life_coplevel >= 1" },
+            { "optic_Aco_smg", "ACO", 2750, 1375, "call life_coplevel >= 1" },
+            { "KA_ANPEQ15_Black_IR", "Laser", 1200, 600, "call life_coplevel >= 1" },
+    
             
             
             //================================
@@ -260,9 +276,21 @@ class WeaponShops {
             { "optic_Arco_blk_F", "", 500, , "call life_coplevel >= 2" },
             { "optic_Hamr", "", 550, , "call life_coplevel >= 2" },
             { "optic_NVS", "", 1250, , "call life_coplevel >= 2" },
-            { "acc_flashlight", "", 100, , "call life_coplevel >= 2" },
             { "CSW_FN57_flashlight_glare_2", "", , "call life_coplevel >= 2" },
-            { "CSW_FN57_silencer1", "", 2750, , "call life_coplevel >= 2" }
+            { "CSW_FN57_silencer1", "", 2750, , "call life_coplevel >= 2" },
+
+            
+            //================================
+            //============ SERGEANT ==========
+            //================================              
+        
+            { "optic_SOS", "", 550, , "call life_coplevel >= 2" },      
+            { "acc_flashlight", "", 50, , "call life_coplevel >= 2" },
+            { "CSW_FN57_laser_green_2", "", 80, , "call life_coplevel >= 2" },
+            { "CSW_FN57_Shield_P", "", 450, , "call life_coplevel >= 2" },          
+            { "muzzle_snds_570", "", 450, , "call life_coplevel >= 2" },            
+            { "CSW_FN57_silencer1", "", 850, , "call life_coplevel >= 2" }                  
+            
 
         
             
