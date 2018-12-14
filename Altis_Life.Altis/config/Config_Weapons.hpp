3a5ee5a28574285f70a83e163276bf658e5a3164
SnaakZA <snaakza@gmail.com>
2018-12-14 07:11:22 +0200

    Weapons LVL 1 - 2 Done

--- Altis_Life.Altis/config/Config_Weapons.hpp
+++ Altis_Life.Altis/config/Config_Weapons.hpp
@@ -6,7 +6,7 @@
 *            in any combination. For example:
 *                "call life_coplevel && license_civ_someLicense"
 *            This will also let you call any other function.
-*            
+*
 *
 *    ARRAY FORMAT:
 *        0: STRING (Classname): Item Classname
@@ -42,6 +42,7 @@ class WeaponShops {
             { "10Rnd_9x21_Mag", "", 250, 125, "" } //Apex DLC
         };
         accs[] = {
+            { "acc_flashlight_pistol", "", 1000, 500, "" },//Pistol Flashlight
             { "optic_ACO_grn_smg", "", 2500, 1250, "" }
         };
     };
@@ -61,9 +62,7 @@ class WeaponShops {
             { "arifle_ARX_blk_F", "", 22000, 11000, "" }, //Apex DLC
             { "arifle_SPAR_01_blk_F", "", 33000, 16500, "" }, //Apex DLC
             { "arifle_CTAR_blk_F", "", 30000, 15000, "" } //Apex DLC
-
-
-                };
+        };
         mags[] = {
             { "30Rnd_556x45_Stanag", "", 300, 150, "" },
             { "30Rnd_762x39_Mag_F", "", 300, 150, "" }, //Apex DLC
@@ -98,6 +97,7 @@ class WeaponShops {
             { "30Rnd_9x21_Mag", "", 250, 125, "" }
         };
         accs[] = {
+            { "acc_flashlight_pistol", "", 500, 250, "" },//Pistol Flashlight
             { "optic_ACO_grn_smg", "", 950, 475, "" }
         };
     };
@@ -114,8 +114,6 @@ class WeaponShops {
             { "ItemCompass", "", 50, 25, "" },
             { "ItemWatch", "", 50, 25, "" },
             { "FirstAidKit", "", 150, 75, "" },
-            //{"tf_anprc152_1","Radio 5km",500},
-            //{"tf_rt1523g_big_rhs","Long range radio ",1500},
             { "NVGoggles", "", 2000, 1000, "" },
             { "Chemlight_red", "", 300, 150, "" },
             { "Chemlight_yellow", "", 300, 150, "" },
@@ -153,6 +151,12 @@ class WeaponShops {
         side = "cop";
         conditions = "";
         items[] = {
+            
+            
+            //================================
+            //============ ALL RANKS =========
+            //================================
+            
             { "Binocular", "", 150, 75, "" },
             { "ItemGPS", "", 100, 50, "" },
             { "FirstAidKit", "", 150, 75, "" },
@@ -160,33 +164,109 @@ class WeaponShops {
             { "HandGrenade_Stone", $STR_W_items_Flashbang, 1700, 850, "" },
             { "hgun_P07_snds_F", $STR_W_items_StunPistol, 2000, 1000, "" },
             { "arifle_SDAR_F", $STR_W_items_TaserRifle, 20000, 10000, "" },
-            //{"tf_anprc152_1","Radio 5km",500},
-            //{"tf_rt1523g_big_rhs","Long range radio ",1500},
-            { "hgun_P07_F", "", 7500, 3750, "" },
-            { "hgun_P07_khk_F", "", 7500, 3750, "" }, //Apex DLC
-            { "hgun_Pistol_heavy_01_F", "", 9500, 4750, "call life_coplevel >= 1" },
-            { "SMG_02_ACO_F", "", 30000, 15000, "call life_coplevel >= 2" },
-            { "arifle_MX_F", "", 35000, 17500, "call life_coplevel >= 2" },
-            { "hgun_ACPC2_F", "", 17500, 8750, "call life_coplevel >= 3" },
-            { "arifle_MXC_F", "", 30000, 15000, "call life_coplevel >= 3" },
-            { "srifle_DMR_07_blk_F", "", 32000, 16000, "call life_coplevel >= 3" } //Apex DLC Sniper
+            
+            
+            
+            
+            //================================
+            //============ CADET  ============
+            //================================
+            
+            
+            { "SMG_02_F", "", 30000, 1500, "call life_coplevel >= 1" },         
+            
+            
+            
+                
+            //================================
+            //============ CORPORAL ==========
+            //================================
+    
+            { "SMG_03C_TR_black", "", 3500, , "call life_coplevel >= 2" },      
+            { "prpl_benelli_pgs_rail", "", 5500, , "call life_coplevel >= 2" }, 
+            { "arifle_CTAR_blk_F", "", 6500, , "call life_coplevel >= 2" },     
+            { "CSW_FN57", "", 5500, , "call life_coplevel >= 2" },      
+            { "SatchelCharge_Remote_Mag", "", 5500, , "call life_coplevel >= 2" },      
+            { "Rangefinder", "", 5500, , "call life_coplevel >= 2" }        
+
+    
+    
+    
+    
+            
+            
         };
         mags[] = {
+            
+            //================================
+            //============ CADET  ============
+            //================================
+            
+            
             { "16Rnd_9x21_Mag", "", 125, 60, "" },
             { "20Rnd_556x45_UW_mag", $STR_W_mags_TaserRifle, 125, 60, "" },
-            { "11Rnd_45ACP_Mag", "", 130, 65, "" },
-            { "30Rnd_65x39_caseless_mag", "", 130, 65, "" },
+            { "30Rnd_9x21_Red_Mag", "", 125, 60, "" },
+            { "30Rnd_9x21_Mag_SMG_02", "", 125, 60, "" },
             { "30Rnd_9x21_Mag", "", 250, 125, "" },
-            { "9Rnd_45ACP_Mag", "", 200, 100, "" },
-            { "20Rnd_650x39_Cased_Mag_F", "", 200, 100, "" } //Apex DLC
+
+            //================================
+            //============ CORPORAL ==========
+            //================================
+
+            { "50Rnd_570x28_SMG_03", "", 200, , "call life_coplevel >= 2" },                
+            { "30Rnd_580x42_Mag_F", "", 200, , "call life_coplevel >= 2" },
+            { "prpl_8Rnd_12Gauge_Pellets", "", 200, , "call life_coplevel >= 2" },          
+            { "prpl_8Rnd_12Gauge_Pellets", "", 200, , "call life_coplevel >= 2" },                      
+            { "CSW_20Rnd_57x28_SS190", "", 200, , "call life_coplevel >= 2" }                   
+            
+
+
+            
+            
+            
+            
+            
+            
+            
+            
         };
         accs[] = {
             { "muzzle_snds_L", "", 650, 325, "" },
-            { "optic_MRD", "", 2750, 1375, "" },
-            { "acc_flashlight", "", 750, 375, "" },
-            { "optic_Holosight", "", 1200, 600, "" },
-            { "optic_Arco", "", 2500, 1250, "" },
-            { "muzzle_snds_H", "", 2750, 1375, "" }
+            { "optic_MRD", "", 2750, 1375, "call life_coplevel >= 1" },
+            { "acc_flashlight_pistol", "", 250, 125, "call life_coplevel >= 1" },//Pistol Flashlight
+            { "acc_flashlight", "", 750, 375, "call life_coplevel >= 2" },
+            { "optic_Holosight", "", 1200, 600, "call life_coplevel >= 2" },
+            { "optic_Arco", "", 2500, 1250, "call life_coplevel >= 2" },
+            { "muzzle_snds_H", "", 2750, 1375, "call life_coplevel >= 2" },
+            
+            
+            
+            //================================
+            //============ CADET  ============
+            //================================
+            
+            { "muzzle_snds_L", "Suppressor 9mm", 650, 325, "" },
+            { "optic_Aco_smg", "ACO", 2750, 1375, "" },
+            { "acc_flashlight", "Gun Light", 750, 375, "" },
+            { "KA_ANPEQ15_Black_IR", "Laser", 1200, 600, "" },
+            
+            
+            
+            //================================
+            //============ CORPORAL ==========
+            //================================          
+                        
+            { "optic_Aco", "", 500, , "call life_coplevel >= 2" },
+            { "optic_Arco_blk_F", "", 500, , "call life_coplevel >= 2" },
+            { "optic_Hamr", "", 550, , "call life_coplevel >= 2" },
+            { "optic_NVS", "", 1250, , "call life_coplevel >= 2" },
+            { "acc_flashlight", "", 100, , "call life_coplevel >= 2" },
+            { "CSW_FN57_flashlight_glare_2", "", , "call life_coplevel >= 2" },
+            { "CSW_FN57_silencer1", "", 2750, , "call life_coplevel >= 2" }
+
+        
+            
+            
         };
     };
 
